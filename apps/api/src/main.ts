355a163a9be7d43736a338752da29780c7087ef0
gando1354 <abdallah.khaled2003@gmail.com>
2025-12-02 23:14:26 +0200

    addingport again

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  await app.listen(4000);
-  console.log('API running on http://localhost:4000');
+  const port = process.env.PORT || 4000;
+  await app.listen(port, () => {
+    console.log(`API running on port ${port}`);
+  });
 }
 bootstrap();
