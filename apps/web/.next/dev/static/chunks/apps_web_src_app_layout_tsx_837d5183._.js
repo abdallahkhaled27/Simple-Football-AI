9de15460f1264910e1f0def15075c00b1e1210c0
gando1354 <abdallah.khaled2003@gmail.com>
2025-12-03 12:39:44 +0200

    Fix API URL

--- apps/web/.next/dev/static/chunks/apps_web_src_app_layout_tsx_837d5183._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_web_src_app_globals_ea8a3ee7.css"
-],
-    source: "dynamic"
-});
