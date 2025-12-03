8f089b41b2ce9e8fa6667b38baa27e9ad2461b4c
gando1354 <abdallah.khaled2003@gmail.com>
2025-12-03 12:43:30 +0200

    enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 4000;
-  await app.listen(port, () => {
-    console.log(`API running on port ${port}`);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://simple-football-ai-1.onrender.com',
+    ],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
   });
+
+  await app.listen(process.env.PORT || 4000);
+  console.log(`API running on port ${process.env.PORT || 4000}`);
 }
 bootstrap();
